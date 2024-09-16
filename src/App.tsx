0c1642086f6d98a1a95c33c8b4647357cba2c81b
Chris Shen <chris.y.shen@gmail.com>
2024-09-16 00:10:54 -0700

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import "./App.css";
-
-import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
-import React from "react";
 
-class App extends React.Component {
-  public render() {
-    return (
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/home" Component={Home} />
-          <Route path="/recipes" Component={Recipes} />
-        </Routes>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <NameContainer href="home">chris shen</NameContainer>
+      <NameContainer to="/">chris shen</NameContainer>
       <NavigationContainer>
-        <HeaderLink href="home">Home</HeaderLink>
-        <HeaderLink href="recipes">Recipes</HeaderLink>
+        <HeaderLink to="/">Home</HeaderLink>
+        <HeaderLink to="/recipes">Recipes</HeaderLink>
       </NavigationContainer>
     </HeaderContainer>
   );
@@ -23,7 +24,7 @@ const HeaderContainer = styled.header`
   // box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
-const NameContainer = styled.a`
+const NameContainer = styled(Link)`
   font-family: "Montserrat", sans-serif;
   font-size: 28px;
   font-weight: bold;
@@ -36,7 +37,7 @@ const NavigationContainer = styled.nav`
   gap: 20px;
 `;
 
-const HeaderLink = styled.a`
+const HeaderLink = styled(Link)`
   font-family: "Montserrat", sans-serif;
   color: #333;
   font-size: 18px;
